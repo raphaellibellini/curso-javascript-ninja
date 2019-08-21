8c55d8331d5a8628e2fbd8cedc71ad0a6887973a
Raphaelli Bellini <rafael.bellini@hotmail.com>
2019-08-21 20:58:47 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,21 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+// let myArray = [ 2, 4, 6, 8, 9];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+/* 
+ * function mostrarArray(array) {
+ *  return array;
+ * }
+ */
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+// console.log(mostrarArray(array)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +24,29 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+/*
+ * function mostrarItemArray(array, index) {
+ *  return array[index];
+ * }
+ */
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+// let mySecondArray = [1, 'var', true, [2, 4, 6], {cor: 'vermelho'}];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+/*
+ * console.log(mostrarItemArray(mySecondArray, 0));
+ * console.log(mostrarItemArray(mySecondArray, 1));
+ * console.log(mostrarItemArray(mySecondArray, 2));
+ * console.log(mostrarItemArray(mySecondArray, 3));
+ * console.log(mostrarItemArray(mySecondArray, 4));
+ */
+ 
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,7 +62,30 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+/*
+ * function book(nomeLivro) {
+ *     let estante = {
+ *         'o ilusionista': {
+ *             quantidadePaginas: 500,
+ *             autor: 'Carlos Maia',
+ *             editora: 'Nova Palavra'
+ *         },
+ *         'o poeta louco': {
+ *             quantidadePaginas: 400,
+ *             autor: 'Heitor Brandi',
+ *             editora: 'Nova Palavra'
+ *         },
+ *         'o atomo': {
+ *             quantidadePaginas: 450,
+ *             autor: 'João Mascaranhas',
+ *             editora: 'Conhecer'
+ *         }
+ *     }
+ * 
+ *     let result = nomeLivro === undefined ? estante : estante[nomeLivro];
+ *     return result;
+ * }
+ */
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
