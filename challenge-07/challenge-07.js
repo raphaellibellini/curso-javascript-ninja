62dd55c4f4f7eb05de3b23be65ef882d10560616
Raphaelli Bellini <rafael.bellini@hotmail.com>
2019-08-23 01:39:22 -0300

    Update challenge-07.js

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -1,45 +1,50 @@
 /*
 Crie um array com 5 items (tipos variados).
 */
-// ?
+// let myArray = [1, 'var', [2, 4, 6], true, {peso: 77}];
 
 /*
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 */
-// ?
+/*
+ * function addItem(item) {
+ *  myArray.push(item);
+ *  return myArray; 
+ * }
+ */
 
 /*
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 */
-// ?
+// console.log(addItem([2,'string', false]));
 
 /*
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-// ?
+// console.log(`O segundo elemento do segundo array é ${myArray[6][1]}.`);
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+// console.log(`O primeiro array tem ${myArray.length} itens.`);
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+// console.log(`O segundo array tem ${myArray[6].length} itens.`);
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
 */
 console.log( 'Números pares entre 10 e 20:' );
-// ?
+/
 
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
